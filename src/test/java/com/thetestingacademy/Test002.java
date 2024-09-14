8fcff776f1332730213124425b6d53308f8e91df
Promode <pramoddutta_96@yahoo.com>
2024-09-14 12:23:03 +0530

    2 files are added

--- src/test/java/com/thetestingacademy/Test002.java
+++ src/test/java/com/thetestingacademy/Test002.java
@@ -1,10 +1,24 @@
 package com.thetestingacademy;
 
+import io.restassured.RestAssured;
+
 public class Test002 {
     public static void main(String[] args) {
+        System.out.println("Rest Assured Test CASE");
+        System.out.println("GET Request Demo");
 
-
-        System.out.println("Hello World!");
+        // Gherkins Syntax
+//        given() - url, headers, body or paylaod
+//        when() - http methods - get, post, patch, put, delete
+//        then() - verify the response - er == ar
+        RestAssured
+                .given()
+                    .baseUri("https://restful-booker.herokuapp.com")
+                    .basePath("/booking/1").log().all()
+                .when()
+                    .get()
+                .then().log().all()
+                    .statusCode(200); // <201> but was <200>.
 
 
     }
